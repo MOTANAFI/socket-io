7d3bc4fb16154525b8dcb251f2e123e3d269516c
LONEWOLF143 <twaha2030@gmail.com>
2025-01-27 16:09:13 +0300

    socket io

---
+++ index.js
@@ -0,0 +1,29 @@
+//1. packages
+import express from 'express';
+import http from 'http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+//2. Instances
+
+const app = express();
+ const server = http.createServer(app);
+ const io = new Server(server);
+
+//3. Serving HTML File
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
+
+//4. Define a connection event handler
+io.on("connection", (socket) => {
+    console.log(socket)
+
+
+})
+
+//5. Start the server
+
+const PORT = 3000
+
+server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
